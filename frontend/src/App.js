3a1aec8ddaa6dc3ff3d5c501bdf99dc924b68f33
Cipriano <natancipriano98@gmail.com>
2020-01-17 22:22:07 -0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import './global.css';
+import './app.css';
+import './sidebar.css';
+
+import Header from './Header';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="app">
+        <aside>
+          <strong>Cadastrar</strong>
+            <form >
+
+                <div className="input-block">
+                  <label htmlFor="github_username">Usuário do github</label>
+                  <input name="github_username" type="text" id="github_username" required/>
+                </div>
+
+                <div className="input-block">
+                  <label htmlFor="techs">Tecnologias</label>
+                  <input name="techs" type="text" id="techs" required/>
+                </div>
+
+               <div className="input-group">
+
+                <div className="input-bolck">
+                    <label htmlFor="longitude">Longitude</label>
+                    <input name="longitude" type="text" id="longitude" required/>
+                </div>
+
+                  <div className="input-bolck">
+                    <label htmlFor="latitude">Latitude</label>
+                    <input name="latitude" type="text" id="latitude" required/>
+                  </div>
+
+               </div>
+
+               <button type="submit">Salvar</button>
+
+               <ul>
+                 <li className="personal-item">
+                    <header>
+                      <img src="teste" alt="Diego Fernandes"/>
+                      <div className="user-info">  {/* 1h */}
+
+                      </div>
+                    </header>
+                 </li>
+               </ul>
+
+
+
+            </form>
+        </aside>
+        <main>
+
+        </main>
+      </div>
+    </>
   );
 }
 
