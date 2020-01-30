f3e0254e0bed06355329106cb3cdf59b663f1d97
Cipriano <natancipriano98@gmail.com>
2020-01-30 06:33:36 -0300

    Adcionado .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,20 @@
-const express = require( 'express' );
-const mongoose = require( 'mongoose' );
-const routes = require( './routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+require("dotenv/config");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://move:ms&l@cluster0-8q706.mongodb.net/move?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useUnifiedTopology : true,   
-})
+mongoose.connect(
+    process.env.MONGO_CONECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
 
-app.use( express.json( ) );
+app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.SERVER_PORT);
 
-
+console.log("Acesse http://localhost:" + process.env.SERVER_PORT);
